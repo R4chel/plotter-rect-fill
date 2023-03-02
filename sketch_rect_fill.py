5fcf5023b5fb8993785ff8a098111d4cee78165e
R4chel <rae84@cornell.edu>
2023-03-02 17:40:46 -0500

    initial attempt

--- sketch_rect_fill.py
+++ sketch_rect_fill.py
@@ -10,6 +10,7 @@ class RectFillSketch(vsketch.SketchClass):
     landscape = vsketch.Param(True)
     pen_width = vsketch.Param(0.7, decimals=3, min_value=1e-10, unit="mm")
     num_layers = vsketch.Param(1)
+    step_count = vsketch.Param(20.)
     # radius = vsketch.Param(1.0, decimals=3, unit="in")
 
     def random_point(self, vsk: vsketch.Vsketch):
@@ -22,10 +23,32 @@ class RectFillSketch(vsketch.SketchClass):
         vsk.translate(self.margin, self.margin)
         vsk.penWidth(f"{self.pen_width}")
 
+        
+        layers = [1 + i for i in range(self.num_layers)]
+
+        step_size = max(self.width, self.height) / self.step_count
+        x0, y0 = step_size, step_size
+        x1 = self.width - step_size
+        y1 = self.height - step_size
+        while x0 <= x1 and y0 <= y1:
+            layer = layers[int(vsk.random(0, len(layers)))]
+            vsk.stroke(layer)
+            match vsk.random(0,1):
+                case rnd if 0 <= rnd < 0.25:
+                    vsk.line(x0, y0, x0, y1)
+                    x0 += step_size
+                case rnd if 0.25 <= rnd < 0.5:
+                    vsk.line(x1, y0, x1, y1)
+                    x1 -= step_size
+                case rnd if 0.5 <= rnd < 0.75:
+                    vsk.line(x0, y0, x1, y0)
+                    y0 += step_size
+                case rnd if 0.75 <= rnd < 1:
+                    vsk.line(x0, y1, x1, y1)
+                    y1 -= step_size
+
+
         # implement your sketch here
-        # layers = [1 + i for i in range(self.num_layers)]
-        # layer = layers[int(vsk.random(0, len(layers)))]
-        # vsk.stroke(layer)
         # vsk.fill(layer)
         # vsk.circle(0, 0, self.radius, mode="radius")
 
